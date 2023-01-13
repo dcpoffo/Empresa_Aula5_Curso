510c23dc493199f1c23ab6601f0e634f8f2fb05d
“Darlan” <dcpoffo@gmail.com>
2023-01-13 20:01:06 -0300

    aumenta em 10% so salarios dos empregados

--- src/Main.java
+++ src/Main.java
@@ -13,11 +13,22 @@ public class Main {
         System.out.printf("Nome do 1º funcionario: %s%n", empregado1.getNome());
         System.out.printf("Salário do 1º funcionario: %.2f%n", empregado1.getSalarioMensal());
         System.out.printf("Ganhos anuais do 1º funcionario: %.2f%n", empregado1.getSalarioMensal()*12);
-
+        System.out.println("");
         System.out.printf("Nome do 2º funcionario: %s%n", empregado2.getNome());
         System.out.printf("Salário do 2º funcionario: %.2f%n", empregado2.getSalarioMensal());
         System.out.printf("Ganhos anuais do 2º funcionario: %.2f%n", empregado2.getSalarioMensal()*12);
 
+        System.out.println("\n********DEPOIS DO AUMENTO DE 10%********\n");
+
+        empregado1.setSalarioMensal(empregado1.getSalarioMensal() * 1.1);
+        System.out.printf("Nome do 1º funcionario: %s%n", empregado1.getNome());
+        System.out.printf("Salário do 1º funcionario: %.2f%n", empregado1.getSalarioMensal());
+        System.out.printf("Ganhos anuais do 1º funcionario: %.2f%n", empregado1.getSalarioMensal()*12);
+        System.out.println("");
+        empregado2.setSalarioMensal(empregado2.getSalarioMensal() * 1.1);
+        System.out.printf("Nome do 2º funcionario: %s%n", empregado2.getNome());
+        System.out.printf("Salário do 2º funcionario: %.2f%n", empregado2.getSalarioMensal());
+        System.out.printf("Ganhos anuais do 2º funcionario: %.2f%n", empregado2.getSalarioMensal()*12);
 
 
         //empregado1.setSalarioMensal();

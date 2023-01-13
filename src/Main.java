f163cb26dde443b84c743e2a00f6a1894d4735f6
“Darlan” <dcpoffo@gmail.com>
2023-01-13 20:07:56 -0300

    cria construtor da classe empregado para inicializar seus atributos

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-        Empregado empregado1 = new Empregado();
-        Empregado empregado2 = new Empregado();
-
-        empregado1.setNome("Darlan R. C. Poffo");
-        empregado1.setSalarioMensal(2500.50);
-
-        empregado2.setNome("Vanessa Y. Tanaka");
-        empregado2.setSalarioMensal(3100.35);
+        Empregado empregado1 = new Empregado(
+                "Darlan R. C. Poffo",
+                2500.50);
+        Empregado empregado2 = new Empregado(
+                "Vanessa Y. Tanaka",
+                3100.35);
 
         System.out.printf("Nome do 1º funcionario: %s%n", empregado1.getNome());
         System.out.printf("Salário do 1º funcionario: %.2f%n", empregado1.getSalarioMensal());

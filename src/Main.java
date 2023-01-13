83d99f4d8efe0a9014721b87ed834b2830dd0a39
“Darlan” <dcpoffo@gmail.com>
2023-01-13 19:42:41 -0300

    cria objetos da classe empregado

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Empregado empregado1 = new Empregado();
+        Empregado empregado2 = new Empregado();
+
+        empregado1.setNome("Darlan R. C. Poffo");
+        empregado1.setSalarioMensal(2500.50);
+
+        empregado2.setNome("Vanessa Y. Tanaka");
+        empregado2.setSalarioMensal(3100.35);
+
+        System.out.printf("Nome do 1º funcionario: %s%n", empregado1.getNome());
+        System.out.printf("Salário do 1º funcionario: %.2f%n", empregado1.getSalarioMensal());
+
+        System.out.printf("Nome do 2º funcionario: %s%n", empregado2.getNome());
+        System.out.printf("Salário do 2º funcionario: %.2f%n", empregado2.getSalarioMensal());
     }
 }
